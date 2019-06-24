c5b4f62824bece610f5159893b71971f5c00a4b5
089245 <sunyongjun@sf-express.com>
2019-06-24 17:58:06 +0800

    init folderadd slf4j log4j/log4j2 loop test code

---
+++ common-error/slf4j-log4j-loop/src/main/java/Slf4jLog4jLoop.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jLog4jLoop {
+    private static Logger logger = LoggerFactory.getLogger(Slf4jLog4jLoop.class);
+
+    public static void main(String[] args) {
+        logger.info("slf4j log4j loop");
+    }
+}
---
+++ common-error/slf4j-log4j2-loop/src/main/java/Slf4jLog4j2Loop.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jLog4j2Loop {
+    private static Logger logger = LoggerFactory.getLogger(Slf4jLog4j2Loop.class);
+
+    public static void main(String[] args) {
+        logger.info("slf4j log4j2 loop");
+    }
+}
